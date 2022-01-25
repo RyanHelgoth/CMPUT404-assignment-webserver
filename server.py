7aa8c6a295af21e36f6a8fe605805ab801e631c2
Ryan Helgoth <78457606+RyanHelgoth@users.noreply.github.com>
2022-01-25 16:33:53 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socketserver
 import os
 
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Ryan Helgoth
+# Copyright 2022 Abram Hindle, Eddie Antonio Santos, Ryan Helgoth
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
